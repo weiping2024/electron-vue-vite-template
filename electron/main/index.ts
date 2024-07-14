c4dccaa85573991d187b5c79675061ffb16daa6f
weipingtang <166967323+weipingtang@users.noreply.github.com>
2024-07-15 01:43:47 +0800

    添加启动服务

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -3,6 +3,7 @@ import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import os from 'node:os'
+import express from 'express'
 
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -37,51 +38,59 @@ if (!app.requestSingleInstanceLock()) {
   app.quit()
   process.exit(0)
 }
+let server;
 
 let win: BrowserWindow | null = null
 const preload = path.join(__dirname, '../preload/index.mjs')
 const indexHtml = path.join(RENDERER_DIST, 'index.html')
 
 async function createWindow() {
-  win = new BrowserWindow({
-    title: 'Main window',
-    icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // nodeIntegration: true,
+  const expressApp = express();
+  expressApp.use(express.static(path.join(process.env.APP_ROOT, 'dist/test')));
 
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      // contextIsolation: false,
-    },
-  })
+  server = expressApp.listen(8899, () =>{
+    win = new BrowserWindow({
+      title: 'Main window',
+      icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
+      webPreferences: {
+        preload,
+        // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+        // nodeIntegration: true,
 
-  if (VITE_DEV_SERVER_URL) { // #298
-    win.loadURL(VITE_DEV_SERVER_URL)
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
-  } else {
-    win.loadFile(indexHtml)
-  }
+        // Consider using contextBridge.exposeInMainWorld
+        // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+        // contextIsolation: false,
+      },
+    })
 
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
+    if (VITE_DEV_SERVER_URL) { // #298
+      win.loadURL(VITE_DEV_SERVER_URL)
+      // Open devTool if the app is not packaged
+      win.webContents.openDevTools()
+    } else {
+      win.loadFile(indexHtml)
+    }
 
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
-  // win.webContents.on('will-navigate', (event, url) => { }) #344
+    // Test actively push message to the Electron-Renderer
+    win.webContents.on('did-finish-load', () => {
+      win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
+
+    // Make all links open with the browser, not with the application
+    // win.webContents.setWindowOpenHandler(({ url }) => {
+    //   if (url.startsWith('https:')) shell.openExternal(url)
+    //   return { action: 'deny' }
+    // })
+    // win.webContents.on('will-navigate', (event, url) => { }) #344
+    })
+
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
   win = null
+  server.close();
   if (process.platform !== 'darwin') app.quit()
 })
 
